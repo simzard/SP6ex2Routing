1b4bc2488b36964210218170cb910adbe51e6023
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-10-22 17:52:23 +0200

    added step 4: created controller

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('SPA',[]);
+var app = angular.module('SPA', []);
+
+app.controller('Control', [function () {
+        
+        var self = this;
+        
+        var persons = [
+            {id: 1, name: "Jens", age: 18}
+            , {id: 2, name: "Peter", age: 23}
+            , {id: 3, name: "Hanne", age: 23}
+        ];
+    }]);
