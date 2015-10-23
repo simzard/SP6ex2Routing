2e5737cd7735a0235e5a6a9ec0d5cbd7b0c5ab62
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-10-23 08:07:12 +0200

    added step 5 and 6: created the views and connected it with the controller

--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,30 @@
-var app = angular.module('SPA', []);
+var app = angular.module('SPA', ['ngRoute']);
+app.controller('HomeController', function ($scope, $routeParams) {
 
-app.controller('Control', [function () {
-        
-        var self = this;
-        
-        var persons = [
-            {id: 1, name: "Jens", age: 18}
-            , {id: 2, name: "Peter", age: 23}
-            , {id: 3, name: "Hanne", age: 23}
-        ];
-    }]);
+    $scope.persons = [
+        {id: 1, name: "Jens", age: 18}
+        , {id: 2, name: "Peter", age: 23}
+        , {id: 3, name: "Hanne", age: 23}
+    ];
+    
+    if(angular.isDefined($routeParams.index)) {
+        var i = $routeParams.index;
+        $scope.person = $scope.persons[i];
+    }
+});
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+            .when('/home', {
+                templateUrl: "views/home.html",
+                controller: "HomeController"
+            })
+            .when('/details/:index', {
+                templateUrl: "views/details.html",
+                controller: "HomeController"
+            })
+            .otherwise({
+                redirectTo: "/home"
+            })
+})
